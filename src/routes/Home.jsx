d4b9d7cdbeb425e0b236d6152563a5214cb161cf
Gregorius-Denmas <6182201098@student.unpar.ac.id>
2025-05-27 14:24:10 +0700

    fix: navbar and minor register page ui

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,8 @@
 import styles from "./home.module.css";
 import poke from "../assets/images/home/home_img.png";
-
+import { useNavigate } from "@solidjs/router";
 function Home() {
+  const navigate = useNavigate();
   return (
     <div class={styles.pageContainer}>
       <section class={styles.heroSection}>
@@ -11,7 +12,7 @@ function Home() {
             The Pokémon TCG website is a platform that provides comprehensive
             information about the Pokémon Trading Card Game.
           </h3>
-          <button class={styles.learnMoreButton}>Learn More</button>
+          <button class={styles.learnMoreButton}  onclick={() => navigate("/about")}>About Us</button>
         </div>
         <img class={styles.imagePlaceholder} src={poke} alt="home_img" />
       </section>
